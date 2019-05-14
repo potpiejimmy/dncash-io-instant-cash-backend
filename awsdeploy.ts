ba6160caca32b7bda3953b3886cd88415ec10936
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2019-05-14 10:12:48 +0200

    fixed aws deploy bundle name

--- awsdeploy.ts
+++ awsdeploy.ts
@@ -1,3 +1,3 @@
 import { spawn } from "child_process";
 
-spawn("eb", ["deploy","-l","dncash-io-"+process.env.npm_package_version], {stdio:'inherit'});
+spawn("eb", ["deploy","-l","dncash-io-instant-cash-"+process.env.npm_package_version], {stdio:'inherit'});
